19b7193ce7af3e1e54d299cbf39af4702c1b763f
Jonathan Dallas <jonathan.dallas@velir.com>
2015-10-06 14:43:00 -0400

    Portfolio - Everest Poker - Laronda Page

---
+++ portfolio-work/ep-laronda/resources/GA-UA-169168-1-5.js
@@ -0,0 +1,179 @@
+var _gaq = _gaq || [];
+
+_gaq.push(['_setAccount', 'UA-169168-1']);
+_gaq.push(['_clearOrganic']);
+
+// -- Google
+_gaq.push(['_addOrganic', 'google.be', 'q']); _gaq.push(['_addOrganic', 'google.cd', 'q']);
+_gaq.push(['_addOrganic', 'google.co.cr', 'q']); _gaq.push(['_addOrganic', 'google.co.je', 'q']);
+_gaq.push(['_addOrganic', 'google.co.ls', 'q']); _gaq.push(['_addOrganic', 'google.co.uk', 'q']);
+_gaq.push(['_addOrganic', 'google.com.br', 'q']); _gaq.push(['_addOrganic', 'google.com.fj', 'q']);
+_gaq.push(['_addOrganic', 'google.com.mt', 'q']); _gaq.push(['_addOrganic', 'google.com.ni', 'q']);
+_gaq.push(['_addOrganic', 'google.com.pe', 'q']); _gaq.push(['_addOrganic', 'google.com.py', 'q']);
+_gaq.push(['_addOrganic', 'google.com.tr', 'q']); _gaq.push(['_addOrganic', 'google.com.uy', 'q']);
+_gaq.push(['_addOrganic', 'google.de', 'q']); _gaq.push(['_addOrganic', 'google.fi', 'q']);
+_gaq.push(['_addOrganic', 'google.gg', 'q']); _gaq.push(['_addOrganic', 'google.hn', 'q']);
+_gaq.push(['_addOrganic', 'google.kz', 'q']); _gaq.push(['_addOrganic', 'google.lu', 'q']);
+_gaq.push(['_addOrganic', 'google.mu', 'q']); _gaq.push(['_addOrganic', 'google.pl', 'q']);
+_gaq.push(['_addOrganic', 'google.rw', 'q']); _gaq.push(['_addOrganic', 'google.bo', 'q']);
+_gaq.push(['_addOrganic', 'google.co.in', 'q']); _gaq.push(['_addOrganic', 'google.com.sa', 'q']);
+_gaq.push(['_addOrganic', 'google.cn', 'q']); _gaq.push(['_addOrganic', 'google.jo', 'q']);
+_gaq.push(['_addOrganic', 'google.at', 'q']); _gaq.push(['_addOrganic', 'google.sk', 'q']);
+_gaq.push(['_addOrganic', 'google.se', 'q']); _gaq.push(['_addOrganic', 'google.ae', 'q']);
+_gaq.push(['_addOrganic', 'google.bi', 'q']); _gaq.push(['_addOrganic', 'google.ch', 'q']);
+_gaq.push(['_addOrganic', 'google.hu', 'q']); _gaq.push(['_addOrganic', 'google.co.jp', 'q']);
+_gaq.push(['_addOrganic', 'google.co.nz', 'q']); _gaq.push(['_addOrganic', 'google.com.ar', 'q']);
+_gaq.push(['_addOrganic', 'google.com.co', 'q']); _gaq.push(['_addOrganic', 'google.com.gr', 'q']);
+_gaq.push(['_addOrganic', 'google.com.mx', 'q']); _gaq.push(['_addOrganic', 'google.com.np', 'q']);
+_gaq.push(['_addOrganic', 'google.com.pk', 'q']); _gaq.push(['_addOrganic', 'google.ru', 'q']);
+_gaq.push(['_addOrganic', 'google.com.tw', 'q']); _gaq.push(['_addOrganic', 'google.com.ve', 'q']);
+_gaq.push(['_addOrganic', 'google.dj', 'q']); _gaq.push(['_addOrganic', 'google.fm', 'q']);
+_gaq.push(['_addOrganic', 'google.gl', 'q']); _gaq.push(['_addOrganic', 'google.ie', 'q']);
+_gaq.push(['_addOrganic', 'google.li', 'q']); _gaq.push(['_addOrganic', 'google.lv', 'q']);
+_gaq.push(['_addOrganic', 'google.mw', 'q']); _gaq.push(['_addOrganic', 'google.pn', 'q']);
+_gaq.push(['_addOrganic', 'google.com.om', 'q']); _gaq.push(['_addOrganic', 'google.co.uz', 'q']);
+_gaq.push(['_addOrganic', 'google.as', 'q']); _gaq.push(['_addOrganic', 'google.hr', 'q']);
+_gaq.push(['_addOrganic', 'google.no', 'q']); _gaq.push(['_addOrganic', 'google.ro', 'q']);
+_gaq.push(['_addOrganic', 'google.co.za', 'q']); _gaq.push(['_addOrganic', 'google.cz', 'q']);
+_gaq.push(['_addOrganic', 'google.at', 'q']); _gaq.push(['_addOrganic', 'google.ca', 'q']);
+_gaq.push(['_addOrganic', 'google.cl', 'q']); _gaq.push(['_addOrganic', 'google.co.il', 'q']);
+_gaq.push(['_addOrganic', 'google.co.kr', 'q']); _gaq.push(['_addOrganic', 'google.co.th', 'q']);
+_gaq.push(['_addOrganic', 'google.com.au', 'q']); _gaq.push(['_addOrganic', 'google.com.do', 'q']);
+_gaq.push(['_addOrganic', 'google.com.hk', 'q']); _gaq.push(['_addOrganic', 'google.com.nf', 'q']);
+_gaq.push(['_addOrganic', 'google.com.pa', 'q']); _gaq.push(['_addOrganic', 'google.com.pr', 'q']);
+_gaq.push(['_addOrganic', 'google.com.sg', 'q']); _gaq.push(['_addOrganic', 'google.com.ua', 'q']);
+_gaq.push(['_addOrganic', 'google.com.vn', 'q']); _gaq.push(['_addOrganic', 'google.dk', 'q']);
+_gaq.push(['_addOrganic', 'google.fr', 'q']); _gaq.push(['_addOrganic', 'google.gm', 'q']);
+_gaq.push(['_addOrganic', 'google.it', 'q']); _gaq.push(['_addOrganic', 'google.lt', 'q']);
+_gaq.push(['_addOrganic', 'google.ms', 'q']); _gaq.push(['_addOrganic', 'google.nl', 'q']);
+_gaq.push(['_addOrganic', 'google.pt', 'q']); _gaq.push(['_addOrganic', 'google.sm', 'q']);
+_gaq.push(['_addOrganic', 'google.vg', 'q']); _gaq.push(['_addOrganic', 'google.ph', 'q']);
+_gaq.push(['_addOrganic', 'google.bg', 'q']); _gaq.push(['_addOrganic', 'google.ec', 'q']);
+_gaq.push(['_addOrganic', 'google.sh', 'q']); _gaq.push(['_addOrganic', 'google.com.sa', 'q']);
+_gaq.push(['_addOrganic', 'google.es', 'q']); _gaq.push(['_addOrganic', 'google.com', 'q']);
+_gaq.push(['_addOrganic', 'google', 'q']);
+
+// -- Yahoo!
+_gaq.push(['_addOrganic', 'fr.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'no.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'sg.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'uk.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'se.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'cf.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'ie.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'au.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'pl.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'de.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'au.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'dk.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'es.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'mx.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'cn.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'nl.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'ch.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'tw.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'ar.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'it.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'br.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'ca.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'ct.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'yahoo.co.jp', 'p']);
+_gaq.push(['_addOrganic', 'in.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'nz.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'chfr.search.yahoo.com', 'p']); _gaq.push(['_addOrganic', 'vn.search.yahoo.com', 'p']);
+_gaq.push(['_addOrganic', 'yahoo.com', 'p']); _gaq.push(['_addOrganic', 'yahoo', 'p']);
+
+// -- MSN
+_gaq.push(['_addOrganic', 'za.msn.com', 'q']); _gaq.push(['_addOrganic', 'it.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'br.msn.com', 'q']); _gaq.push(['_addOrganic', 'jp.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'fi.msn.com', 'q']); _gaq.push(['_addOrganic', 'se.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'nz.msn.com', 'q']); _gaq.push(['_addOrganic', 'de.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'latam.msn.com', 'q']); _gaq.push(['_addOrganic', 'in.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'fr.msn.com', 'q']); _gaq.push(['_addOrganic', 'be.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'cn.msn.com', 'q']); _gaq.push(['_addOrganic', 'sg.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'mx.msn.com', 'q']); _gaq.push(['_addOrganic', 'ch.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'kr.msn.com', 'q']); _gaq.push(['_addOrganic', 'be.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'au.msn.com', 'q']); _gaq.push(['_addOrganic', 'nl.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'ie.msn.com', 'q']); _gaq.push(['_addOrganic', 'il.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'uk.msn.com', 'q']); _gaq.push(['_addOrganic', 'ca.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'tw.msn.com', 'q']); _gaq.push(['_addOrganic', 'hk.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'at.msn.com', 'q']); _gaq.push(['_addOrganic', 'dk.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'my.msn.com', 'q']); _gaq.push(['_addOrganic', 'es.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'arabia.msn.com', 'q']); _gaq.push(['_addOrganic', 'no.msn.com', 'q']);
+_gaq.push(['_addOrganic', 'msn.com', 'q']); _gaq.push(['_addOrganic', 'msn', 'q']);
+
+// -- AltaVista
+_gaq.push(['_addOrganic', 'fr.altavista.com', 'q']); _gaq.push(['_addOrganic', 'ie.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'nl.altavista.com', 'q']); _gaq.push(['_addOrganic', 'pt.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'uk.altavista.com', 'q']); _gaq.push(['_addOrganic', 'be.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'mx.altavista.com', 'q']); _gaq.push(['_addOrganic', 'it.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'de.altavista.com', 'q']); _gaq.push(['_addOrganic', 'kr.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'at.altavista.com', 'q']); _gaq.push(['_addOrganic', 'es.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'ca.altavista.com', 'q']); _gaq.push(['_addOrganic', 'in.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'au.altavista.com', 'q']); _gaq.push(['_addOrganic', 'dk.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'nz.altavista.com', 'q']); _gaq.push(['_addOrganic', 'fi.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'se.altavista.com', 'q']); _gaq.push(['_addOrganic', 'ch.altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'br.altavista.com', 'q']); _gaq.push(['_addOrganic', 'altavista.com', 'q']);
+_gaq.push(['_addOrganic', 'altavista', 'q']);
+
+// -- Lycos
+_gaq.push(['_addOrganic', 'lycos.it', 'query']); _gaq.push(['_addOrganic', 'lycos.co.uk', 'query']);
+_gaq.push(['_addOrganic', 'lycos.ch', 'query']); _gaq.push(['_addOrganic', 'lycos.fr', 'query']);
+_gaq.push(['_addOrganic', 'lycos.nl', 'query']); _gaq.push(['_addOrganic', 'lycos.at', 'query']);
+_gaq.push(['_addOrganic', 'lycos.de', 'query']); _gaq.push(['_addOrganic', 'lycos.es', 'query']);
+_gaq.push(['_addOrganic', 'lycos.com', 'query']); _gaq.push(['_addOrganic', 'lycos', 'query']);
+
+// -- LookSmart
+_gaq.push(['_addOrganic', 'looksmart.ca', 'qt']); _gaq.push(['_addOrganic', 'looksmart.com.au', 'qt']);
+_gaq.push(['_addOrganic', 'looksmart.co.uk', 'qt']); _gaq.push(['_addOrganic', 'looksmart.co.jp', 'qt']);
+_gaq.push(['_addOrganic', 'looksmart.com', 'qt']); _gaq.push(['_addOrganic', 'looksmart', 'qt']);
+
+// -- Ask
+_gaq.push(['_addOrganic', 'de.ask.com', 'q']); _gaq.push(['_addOrganic', 'uk.ask.com', 'q']);
+_gaq.push(['_addOrganic', 'it.ask.com', 'q']); _gaq.push(['_addOrganic', 'fr.ask.com', 'q']);
+_gaq.push(['_addOrganic', 'jp.ask.com', 'q']); _gaq.push(['_addOrganic', 'ask.com', 'q']);
+_gaq.push(['_addOrganic', 'ask.com', 'q']); _gaq.push(['_addOrganic', 'ask', 'q']);
+
+// -- AOL
+_gaq.push(['_addOrganic', 'aol.de', 'encquery']); _gaq.push(['_addOrganic', 'aol.co.uk', 'encquery']);
+_gaq.push(['_addOrganic', 'aol.com.mx', 'encquery']); _gaq.push(['_addOrganic', 'aol.fr', 'encquery']);
+_gaq.push(['_addOrganic', 'aol.com', 'encquery']); _gaq.push(['_addOrganic', 'aol', 'encquery']);
+_gaq.push(['_addOrganic', 'aol.de', 'query']); _gaq.push(['_addOrganic', 'aol.co.uk', 'query']);
+_gaq.push(['_addOrganic', 'aol.com.mx', 'query']); _gaq.push(['_addOrganic', 'aol.fr', 'query']);
+_gaq.push(['_addOrganic', 'aol.com', 'query']); _gaq.push(['_addOrganic', 'aol', 'query']);
+
+// -- Bluewin
+_gaq.push(['_addOrganic', 'de.bluewin.ch', 'query']); _gaq.push(['_addOrganic', 'it.bluewin.ch', 'query']);
+_gaq.push(['_addOrganic', 'fr.bluewin.ch', 'query']); _gaq.push(['_addOrganic', 'bluewin.ch', 'query']);
+_gaq.push(['_addOrganic', 'bluewin.com', 'query']); _gaq.push(['_addOrganic', 'bluewin', 'query']);
+
+// -- Hotbot
+_gaq.push(['_addOrganic', 'hotbot.de', 'query']); _gaq.push(['_addOrganic', 'hotbot.co.uk', 'query']);
+_gaq.push(['_addOrganic', 'hotbot.nl', 'query']); _gaq.push(['_addOrganic', 'hotbot.fr', 'query']);
+_gaq.push(['_addOrganic', 'hotbot.es', 'query']); _gaq.push(['_addOrganic', 'hotbot.com', 'query']);
+_gaq.push(['_addOrganic', 'hotbot', 'query']);
+
+// -- AllTheWeb
+_gaq.push(['_addOrganic', 'fr.alltheweb.com', 'q']); _gaq.push(['_addOrganic', 'nl.alltheweb.com', 'q']);
+_gaq.push(['_addOrganic', 'alltheweb.com', 'q']); _gaq.push(['_addOrganic', 'alltheweb', 'q']);
+
+// -- Voila
+_gaq.push(['_addOrganic', 'voila.fr', 'rdata']); _gaq.push(['_addOrganic', 'voila.com', 'rdata']);
+_gaq.push(['_addOrganic', 'voila', 'rdata']);
+
+// -- Single Listings
+_gaq.push(['_addOrganic', 'arbo', 'keywords']); _gaq.push(['_addOrganic', 'del.icio.us', 'p']);
+_gaq.push(['_addOrganic', 'exactseek', 'q']); _gaq.push(['_addOrganic', 'icerocket', 'q']);
+_gaq.push(['_addOrganic', 'iwon', 'q']); _gaq.push(['_addOrganic', 'infoseek', 'p']);
+_gaq.push(['_addOrganic', 'szukacz.pl', 'q']); _gaq.push(['_addOrganic', 'toile', 'q']);
+_gaq.push(['_addOrganic', 'uknetguide', 'search']); _gaq.push(['_addOrganic', 'websearch', 'qkw']);
+_gaq.push(['_addOrganic', 'arianna', 'query']); _gaq.push(['_addOrganic', 'clusty', 'query']);
+_gaq.push(['_addOrganic', 'fresheye', 'kw']);
+_gaq.push(['_addOrganic', 'go.com', 'p']); _gaq.push(['_addOrganic', 'llse', 'search_for']);
+_gaq.push(['_addOrganic', 'virgilio', 'qs']); _gaq.push(['_addOrganic', 'webwombat', 'ix']);
+_gaq.push(['_addOrganic', 'alexa', 'q']); _gaq.push(['_addOrganic', 'ananzi', 'qt']);
+_gaq.push(['_addOrganic', 'dogpile', 'q']); _gaq.push(['_addOrganic', 'entireweb', 'q']);
+_gaq.push(['_addOrganic', 'euroseek', 'string']); _gaq.push(['_addOrganic', 'teoma', 'q']);
+_gaq.push(['_addOrganic', 'bing', 'q']);
+
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var cookies = EP.read_cookies();
+    if (cookies.a) {
+      _gaq.push(['_setCustomVar', 1, 'Advertiser ID', cookies.a, 1 ]);
+    }
+})();
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
