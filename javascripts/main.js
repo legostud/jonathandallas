3667b9bf954ed00c8e121b99f39768ca8f80df47
Jonathan Dallas <jonathan.dallas@velir.com>
2015-10-06 17:35:03 -0400

    Jonathan - Base Site files

---
+++ javascripts/main.js
@@ -0,0 +1,16 @@
+(function(window, document, $, undefined) {
+
+var MAIN = (function() {
+	
+	function init() {
+
+	}
+	
+	return {
+		init : init
+	};
+})();
+
+MAIN.init();
+
+}(window, document, jQuery));
