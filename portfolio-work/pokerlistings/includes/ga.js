b19d926453a2352013d2adc8820d57d50e6c7e63
Jonathan Dallas <jonathan.dallas@velir.com>
2015-10-06 14:52:28 -0400

    Portfolio - Poker Listing

---
+++ portfolio-work/pokerlistings/includes/ga.js
@@ -0,0 +1,166 @@
+// pageTracker._setDomainName("none");
+pageTracker._clearOrganic();
+
+// -- Google
+pageTracker._addOrganic("google.be","q"); pageTracker._addOrganic("google.cd","q");
+pageTracker._addOrganic("google.co.cr","q"); pageTracker._addOrganic("google.co.je","q");
+pageTracker._addOrganic("google.co.ls","q"); pageTracker._addOrganic("google.co.uk","q");
+pageTracker._addOrganic("google.com.br","q"); pageTracker._addOrganic("google.com.fj","q");
+pageTracker._addOrganic("google.com.mt","q"); pageTracker._addOrganic("google.com.ni","q");
+pageTracker._addOrganic("google.com.pe","q"); pageTracker._addOrganic("google.com.py","q");
+pageTracker._addOrganic("google.com.tr","q"); pageTracker._addOrganic("google.com.uy","q");
+pageTracker._addOrganic("google.de","q"); pageTracker._addOrganic("google.fi","q");
+pageTracker._addOrganic("google.gg","q"); pageTracker._addOrganic("google.hn","q");
+pageTracker._addOrganic("google.kz","q"); pageTracker._addOrganic("google.lu","q");
+pageTracker._addOrganic("google.mu","q"); pageTracker._addOrganic("google.pl","q");
+pageTracker._addOrganic("google.rw","q"); pageTracker._addOrganic("google.bo","q");
+pageTracker._addOrganic("google.co.in","q"); pageTracker._addOrganic("google.com.sa","q");
+pageTracker._addOrganic("google.cn","q"); pageTracker._addOrganic("google.jo","q");
+pageTracker._addOrganic("google.at","q"); pageTracker._addOrganic("google.sk","q");
+pageTracker._addOrganic("google.se","q"); pageTracker._addOrganic("google.ae","q");
+pageTracker._addOrganic("google.bi","q"); pageTracker._addOrganic("google.ch","q");
+pageTracker._addOrganic("google.hu","q"); pageTracker._addOrganic("google.co.jp","q");
+pageTracker._addOrganic("google.co.nz","q"); pageTracker._addOrganic("google.com.ar","q");
+pageTracker._addOrganic("google.com.co","q"); pageTracker._addOrganic("google.com.gr","q");
+pageTracker._addOrganic("google.com.mx","q"); pageTracker._addOrganic("google.com.np","q");
+pageTracker._addOrganic("google.com.pk","q"); pageTracker._addOrganic("google.ru","q");
+pageTracker._addOrganic("google.com.tw","q"); pageTracker._addOrganic("google.com.ve","q");
+pageTracker._addOrganic("google.dj","q"); pageTracker._addOrganic("google.fm","q");
+pageTracker._addOrganic("google.gl","q"); pageTracker._addOrganic("google.ie","q");
+pageTracker._addOrganic("google.li","q"); pageTracker._addOrganic("google.lv","q");
+pageTracker._addOrganic("google.mw","q"); pageTracker._addOrganic("google.pn","q");
+pageTracker._addOrganic("google.com.om","q"); pageTracker._addOrganic("google.co.uz","q");
+pageTracker._addOrganic("google.as","q"); pageTracker._addOrganic("google.hr","q");
+pageTracker._addOrganic("google.no","q"); pageTracker._addOrganic("google.ro","q");
+pageTracker._addOrganic("google.co.za","q"); pageTracker._addOrganic("google.cz","q");
+pageTracker._addOrganic("google.at","q"); pageTracker._addOrganic("google.ca","q");
+pageTracker._addOrganic("google.cl","q"); pageTracker._addOrganic("google.co.il","q");
+pageTracker._addOrganic("google.co.kr","q"); pageTracker._addOrganic("google.co.th","q");
+pageTracker._addOrganic("google.com.au","q"); pageTracker._addOrganic("google.com.do","q");
+pageTracker._addOrganic("google.com.hk","q"); pageTracker._addOrganic("google.com.nf","q");
+pageTracker._addOrganic("google.com.pa","q"); pageTracker._addOrganic("google.com.pr","q");
+pageTracker._addOrganic("google.com.sg","q"); pageTracker._addOrganic("google.com.ua","q");
+pageTracker._addOrganic("google.com.vn","q"); pageTracker._addOrganic("google.dk","q");
+pageTracker._addOrganic("google.fr","q"); pageTracker._addOrganic("google.gm","q");
+pageTracker._addOrganic("google.it","q"); pageTracker._addOrganic("google.lt","q");
+pageTracker._addOrganic("google.ms","q"); pageTracker._addOrganic("google.nl","q");
+pageTracker._addOrganic("google.pt","q"); pageTracker._addOrganic("google.sm","q");
+pageTracker._addOrganic("google.vg","q"); pageTracker._addOrganic("google.ph","q");
+pageTracker._addOrganic("google.bg","q"); pageTracker._addOrganic("google.ec","q");
+pageTracker._addOrganic("google.sh","q"); pageTracker._addOrganic("google.com.sa","q");
+pageTracker._addOrganic("google.es","q"); pageTracker._addOrganic("google.com","q");
+pageTracker._addOrganic("google","q");
+
+// -- Yahoo!
+pageTracker._addOrganic("fr.search.yahoo.com","p"); pageTracker._addOrganic("no.search.yahoo.com","p");
+pageTracker._addOrganic("sg.search.yahoo.com","p"); pageTracker._addOrganic("uk.search.yahoo.com","p");
+pageTracker._addOrganic("se.search.yahoo.com","p"); pageTracker._addOrganic("cf.search.yahoo.com","p");
+pageTracker._addOrganic("ie.search.yahoo.com","p"); pageTracker._addOrganic("au.search.yahoo.com","p");
+pageTracker._addOrganic("pl.search.yahoo.com","p"); pageTracker._addOrganic("de.search.yahoo.com","p");
+pageTracker._addOrganic("au.search.yahoo.com","p"); pageTracker._addOrganic("dk.search.yahoo.com","p");
+pageTracker._addOrganic("es.search.yahoo.com","p"); pageTracker._addOrganic("mx.search.yahoo.com","p");
+pageTracker._addOrganic("cn.search.yahoo.com","p"); pageTracker._addOrganic("nl.search.yahoo.com","p");
+pageTracker._addOrganic("ch.search.yahoo.com","p"); pageTracker._addOrganic("tw.search.yahoo.com","p");
+pageTracker._addOrganic("ar.search.yahoo.com","p"); pageTracker._addOrganic("it.search.yahoo.com","p");
+pageTracker._addOrganic("br.search.yahoo.com","p"); pageTracker._addOrganic("ca.search.yahoo.com","p");
+pageTracker._addOrganic("ct.search.yahoo.com","p"); pageTracker._addOrganic("yahoo.co.jp","p");
+pageTracker._addOrganic("in.search.yahoo.com","p"); pageTracker._addOrganic("nz.search.yahoo.com","p");
+pageTracker._addOrganic("chfr.search.yahoo.com","p"); pageTracker._addOrganic("vn.search.yahoo.com","p");
+pageTracker._addOrganic("yahoo.com","p"); pageTracker._addOrganic("yahoo","p");
+
+// -- MSN
+pageTracker._addOrganic("za.msn.com","q"); pageTracker._addOrganic("it.msn.com","q");
+pageTracker._addOrganic("br.msn.com","q"); pageTracker._addOrganic("jp.msn.com","q");
+pageTracker._addOrganic("fi.msn.com","q"); pageTracker._addOrganic("se.msn.com","q");
+pageTracker._addOrganic("nz.msn.com","q"); pageTracker._addOrganic("de.msn.com","q");
+pageTracker._addOrganic("latam.msn.com","q"); pageTracker._addOrganic("in.msn.com","q");
+pageTracker._addOrganic("fr.msn.com","q"); pageTracker._addOrganic("be.msn.com","q");
+pageTracker._addOrganic("cn.msn.com","q"); pageTracker._addOrganic("sg.msn.com","q");
+pageTracker._addOrganic("mx.msn.com","q"); pageTracker._addOrganic("ch.msn.com","q");
+pageTracker._addOrganic("kr.msn.com","q"); pageTracker._addOrganic("be.msn.com","q");
+pageTracker._addOrganic("au.msn.com","q"); pageTracker._addOrganic("nl.msn.com","q");
+pageTracker._addOrganic("ie.msn.com","q"); pageTracker._addOrganic("il.msn.com","q");
+pageTracker._addOrganic("uk.msn.com","q"); pageTracker._addOrganic("ca.msn.com","q");
+pageTracker._addOrganic("tw.msn.com","q"); pageTracker._addOrganic("hk.msn.com","q");
+pageTracker._addOrganic("at.msn.com","q"); pageTracker._addOrganic("dk.msn.com","q");
+pageTracker._addOrganic("my.msn.com","q"); pageTracker._addOrganic("es.msn.com","q");
+pageTracker._addOrganic("arabia.msn.com","q"); pageTracker._addOrganic("no.msn.com","q");
+pageTracker._addOrganic("msn.com","q"); pageTracker._addOrganic("msn","q");
+
+// -- AltaVista
+pageTracker._addOrganic("fr.altavista.com","q"); pageTracker._addOrganic("ie.altavista.com","q");
+pageTracker._addOrganic("nl.altavista.com","q"); pageTracker._addOrganic("pt.altavista.com","q");
+pageTracker._addOrganic("uk.altavista.com","q"); pageTracker._addOrganic("be.altavista.com","q");
+pageTracker._addOrganic("mx.altavista.com","q"); pageTracker._addOrganic("it.altavista.com","q");
+pageTracker._addOrganic("de.altavista.com","q"); pageTracker._addOrganic("kr.altavista.com","q");
+pageTracker._addOrganic("at.altavista.com","q"); pageTracker._addOrganic("es.altavista.com","q");
+pageTracker._addOrganic("ca.altavista.com","q"); pageTracker._addOrganic("in.altavista.com","q");
+pageTracker._addOrganic("au.altavista.com","q"); pageTracker._addOrganic("dk.altavista.com","q");
+pageTracker._addOrganic("nz.altavista.com","q"); pageTracker._addOrganic("fi.altavista.com","q");
+pageTracker._addOrganic("se.altavista.com","q"); pageTracker._addOrganic("ch.altavista.com","q");
+pageTracker._addOrganic("br.altavista.com","q"); pageTracker._addOrganic("altavista.com","q");
+pageTracker._addOrganic("altavista","q");
+
+// -- Lycos
+pageTracker._addOrganic("lycos.it","query"); pageTracker._addOrganic("lycos.co.uk","query");
+pageTracker._addOrganic("lycos.ch","query"); pageTracker._addOrganic("lycos.fr","query");
+pageTracker._addOrganic("lycos.nl","query"); pageTracker._addOrganic("lycos.at","query");
+pageTracker._addOrganic("lycos.de","query"); pageTracker._addOrganic("lycos.es","query");
+pageTracker._addOrganic("lycos.com","query"); pageTracker._addOrganic("lycos","query");
+
+// -- LookSmart
+pageTracker._addOrganic("looksmart.ca","qt"); pageTracker._addOrganic("looksmart.com.au","qt");
+pageTracker._addOrganic("looksmart.co.uk","qt"); pageTracker._addOrganic("looksmart.co.jp","qt");
+pageTracker._addOrganic("looksmart.com","qt"); pageTracker._addOrganic("looksmart","qt");
+
+// -- Ask
+pageTracker._addOrganic("de.ask.com","q"); pageTracker._addOrganic("uk.ask.com","q");
+pageTracker._addOrganic("it.ask.com","q"); pageTracker._addOrganic("fr.ask.com","q");
+pageTracker._addOrganic("jp.ask.com","q"); pageTracker._addOrganic("ask.com","q");
+pageTracker._addOrganic("ask.com","q"); pageTracker._addOrganic("ask","q");
+
+// -- AOL
+pageTracker._addOrganic("aol.de","encquery"); pageTracker._addOrganic("aol.co.uk","encquery");
+pageTracker._addOrganic("aol.com.mx","encquery"); pageTracker._addOrganic("aol.fr","encquery");
+pageTracker._addOrganic("aol.com","encquery"); pageTracker._addOrganic("aol","encquery");
+pageTracker._addOrganic("aol.de","query"); pageTracker._addOrganic("aol.co.uk","query");
+pageTracker._addOrganic("aol.com.mx","query"); pageTracker._addOrganic("aol.fr","query");
+pageTracker._addOrganic("aol.com","query"); pageTracker._addOrganic("aol","query");
+
+// -- Bluewin
+pageTracker._addOrganic("de.bluewin.ch","query"); pageTracker._addOrganic("it.bluewin.ch","query");
+pageTracker._addOrganic("fr.bluewin.ch","query"); pageTracker._addOrganic("bluewin.ch","query");
+pageTracker._addOrganic("bluewin.com","query"); pageTracker._addOrganic("bluewin","query");
+
+// -- Hotbot
+pageTracker._addOrganic("hotbot.de","query"); pageTracker._addOrganic("hotbot.co.uk","query");
+pageTracker._addOrganic("hotbot.nl","query"); pageTracker._addOrganic("hotbot.fr","query");
+pageTracker._addOrganic("hotbot.es","query"); pageTracker._addOrganic("hotbot.com","query");
+pageTracker._addOrganic("hotbot","query");
+
+// -- AllTheWeb
+pageTracker._addOrganic("fr.alltheweb.com","q"); pageTracker._addOrganic("nl.alltheweb.com","q");
+pageTracker._addOrganic("alltheweb.com","q"); pageTracker._addOrganic("alltheweb","q");
+
+// -- Voila
+pageTracker._addOrganic("voila.fr","rdata"); pageTracker._addOrganic("voila.com","rdata");
+pageTracker._addOrganic("voila","rdata");
+
+// -- Single Listings
+pageTracker._addOrganic("arbo","keywords"); pageTracker._addOrganic("del.icio.us","p");
+pageTracker._addOrganic("exactseek","q"); pageTracker._addOrganic("icerocket","q");
+pageTracker._addOrganic("iwon","q"); pageTracker._addOrganic("infoseek","p");
+pageTracker._addOrganic("szukacz.pl","q"); pageTracker._addOrganic("toile","q");
+pageTracker._addOrganic("uknetguide","search"); pageTracker._addOrganic("websearch","qkw");
+pageTracker._addOrganic("arianna","query"); pageTracker._addOrganic("clusty","query");
+pageTracker._addOrganic("fresheye","kw");
+pageTracker._addOrganic("go.com","p"); pageTracker._addOrganic("llse","search_for");
+pageTracker._addOrganic("virgilio","qs"); pageTracker._addOrganic("webwombat","ix");
+pageTracker._addOrganic("alexa","q"); pageTracker._addOrganic("ananzi","qt");
+pageTracker._addOrganic("dogpile","q"); pageTracker._addOrganic("entireweb","q");
+pageTracker._addOrganic("euroseek","string"); pageTracker._addOrganic("teoma","q");
+pageTracker._addOrganic("bing","q");
+
+// -- Do not edit below
+// pageTracker._initData(); Deprecated. initData() now executes automatically in the ga.js tracking code.
+pageTracker._trackPageview();
