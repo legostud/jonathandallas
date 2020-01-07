4a7105afce78ca97373c2a8f95b805b07c5333d6
Jonathan Dallas <jonathan.dallas@velir.com>
2020-01-07 14:38:05 -0500

    Working version of recaptcha

--- assets-source/js/index.js
+++ assets-source/js/index.js
@@ -4,30 +4,33 @@ GlobalHeader(document, window);
 import Forms from "./forms.js";
 Forms(document, window);
 
-const recaptchaEl = document.querySelector(".js-recaptcha");
-console.log("loading", recaptchaEl);
-if (recaptchaEl) {
-  recaptchaEl.onload = () => {
-    console.log("loaded recaptcha");
-    if (grecaptcha) {
-      console.log("gre", grecaptcha);
-      grecaptcha.ready(function() {
-        console.log("gre ready");
-        grecaptcha
-          .execute("6LeYf8sUAAAAAJS-lw76lzcr-UoyCB6iqZP9CbJ9", {
-            action: "homepage",
-          })
-          .then(function(token) {
-            const formEl = document.querySelector("form");
-            if (formEl) {
-              const inputEl = document.createElement("input");
-              inputEl.setAttribute("type", "hidden");
-              inputEl.setAttribute("name", "recaptcha");
-              inputEl.setAttribute("value", token);
-              formEl.append(inputEl);
-            }
-          });
-      });
-    }
-  };
+const initRecaptcha = () => {
+  if (grecaptcha) {
+    grecaptcha.ready(function() {
+      grecaptcha
+        .execute("6LeYf8sUAAAAAJS-lw76lzcr-UoyCB6iqZP9CbJ9", {
+          action: "homepage",
+        })
+        .then(function(token) {
+          const formEl = document.querySelector("form");
+          if (formEl) {
+            const inputEl = document.createElement("input");
+            inputEl.setAttribute("type", "hidden");
+            inputEl.setAttribute("name", "recaptcha");
+            inputEl.setAttribute("value", token);
+            formEl.append(inputEl);
+          }
+        });
+    });
+  }
+};
+
+if (typeof grecaptcha !== "undefined") {
+  initRecaptcha();
+}
+else {
+  const recaptchaEl = document.querySelector(".js-recaptcha");
+  if (recaptchaEl) {
+    recaptchaEl.onload = initRecaptcha;
+  }
 }
--- contact/contact.php
+++ contact/contact.php
@@ -2,26 +2,49 @@
 header('Access-Control-Allow-Origin: http://jonathandallas.com');
 header('Content-type: application/json');
 
-if ($_POST && &_POST["recaptcha"] ) {
+if ($_POST && $_POST["recaptcha"] ) {
 	// set response code - 200 OK
 	http_response_code(200);
 
-	$subject = "Jonathan Dallas Enquiry Form";
-	$to = "contact-form@jonathandallas.com";
-	$from = $_POST["email"];
+	$captcha = $_POST["recaptcha"];
+	$secretKey = "6LeYf8sUAAAAAOE1tUUg4ciBb8f8q9OgJTa83bWb";
+	$ip = $_SERVER['REMOTE_ADDR'];
 
-	// data
-	$msg = $_POST["name"] . "\r\n" . $_POST["message"];
+  // post request to server
+  $url = 'https://www.google.com/recaptcha/api/siteverify';
+  $data = array('secret' => $secretKey, 'response' => $captcha);
 
-	// Headers
-	$headers = "MIME-Version: 1.0\r\n";
-	$headers.= "Content-type: text/html; charset=UTF-8\r\n";
-	$headers.= "From: <" . $from . ">";
-	mail($to, $subject, $msg, $headers);
+  $options = array(
+    'http' => array(
+      'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+      'method'  => 'POST',
+      'content' => http_build_query($data)
+    )
+  );
+  $context  = stream_context_create($options);
+  $response = file_get_contents($url, false, $context);
 
-	// echo json_response(200,'working');
-	echo json_encode(["IsValid" => true]);
+  $responseKeys = json_decode($response,true);
 
+  if($responseKeys["success"]) {
+		$subject = "Jonathan Dallas Enquiry Form";
+		$to = "contact-form@jonathandallas.com";
+		$from = "contact-form@jonathandallas.com";
+
+		// data
+		$msg = $_POST["name"] . " (" . $_POST["email"] . ") " . "\r\n" . $_POST["message"];
+
+		// Headers
+		$headers = "MIME-Version: 1.0\r\n";
+		$headers.= "Content-type: text/html; charset=UTF-8\r\n";
+		$headers.= "From: <" . $from . ">";
+		mail($to, $subject, $msg, $headers);
+
+		// echo json_response(200,'working');
+		echo json_encode(["IsValid" => true]);
+  } else {
+		echo json_encode(["IsValid" => false, "Message" => "Bad Recaptcha"]);
+  }
 } else {
 
 	// tell the user about error
